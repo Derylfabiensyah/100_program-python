5c03ff9e62e382b6535f838ae1dea8b25dccceaa
= <=>
2024-10-08 21:06:38 +0700

    Program keliling segi lima

---
+++ segiLima.py
@@ -0,0 +1,15 @@
+# dibuat oleh : m.deryl fabiensyah
+# tanggal di buat : 07/10/2024
+# program keliling segi lima
+
+def segiLima():
+    print('================================')
+    print('====== KELILING SEGI LIMA ======')
+    print('================================')
+
+    s = int(input('Masukan panjang sisi nya : '))
+    l = 5 * s
+
+    print(f'keliling : {l}')
+
+segiLima()
