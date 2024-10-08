7721b79c5024f12134da8188e1e0f9edf4ec70da
= <=>
2024-10-08 21:03:39 +0700

    Program persegi Panjang

---
+++ persegi_panjang.py
@@ -0,0 +1,19 @@
+# dibuat oleh : m.deryl fabiensyah
+# tanggal di buat : 24/09/2024
+# program persegi panjang
+
+def persegiPanjang():
+    print('===============================')
+    print('======= PERSEGI PANJANG =======')
+    print('===============================')
+
+    p = float(input('Masukan panjangnya : '))
+    l = float(input('Masukan lebarnya : '))
+
+    rl = lambda p,l : p * l
+    rk = lambda p,l : 2 * (p * l)
+
+    print(f'Luas\t\t: {rl(p,l)}')
+    print(f'Keliling\t: {rk(p,l)}')
+
+persegiPanjang()
