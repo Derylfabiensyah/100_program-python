472cffec3af18bb588c72dad387510263b0c759f
= <=>
2024-10-08 21:01:33 +0700

    Program limas segitiga

---
+++ limas_segitiga.py
@@ -0,0 +1,20 @@
+# dibuat oleh : m.deryl fabiensyah
+# tanggal di buat : 24/09/2024
+# program limas segitiga
+
+def limasSegitiga():
+    print('==============================')
+    print('======= LIMAS SEGITIGA =======')
+    print('==============================')
+
+    la = float(input('Masukan luas alas limas : '))
+    t = float(input('Masukan tinggi limas : '))
+    st = float(input('Masukan luas sisi tegak limas : '))
+
+    lp = lambda la,st : la + st
+    v = lambda la,t : 1 / 3 * la * t
+
+    print(f'Volume\t: {v(la,t)}')
+    print(f'Luas Permukaan\t: {lp(la,st)}')
+
+limasSegitiga()
