935b35f7de1642bb3dceb46f7e887c7d963bf457
= <=>
2024-10-08 21:01:57 +0700

    Program lingkaran

---
+++ lingkaran.py
@@ -0,0 +1,18 @@
+# dibuat oleh : m.deryl fabiensyah
+# tanggal di buat : 10/09/2024
+# program lingkaran
+
+def lingkaran():
+    print('=============================')
+    print('===== PROGRAM LINGKARAN =====')
+    print('=============================\n')
+
+
+    r = float(input('Masukan jari-jari : '))
+    luas = lambda r : 3.14 * r * r
+    keliling = lambda r : 2 * 3.14 * r
+
+    print(f'luas : {luas(r)} cm2')
+    print(f'Keliling : {keliling(r)} cm2\n')
+
+lingkaran()
