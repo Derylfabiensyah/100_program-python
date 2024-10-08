89a7e5160f2add64b12add6aad62884cbe274c57
= <=>
2024-10-08 21:17:45 +0700

    Program segitiga

---
+++ segitiga.py
@@ -0,0 +1,19 @@
+# dibuat oleh : m.deryl fabiensyah
+# tanggal di buat : 24/09/2024
+# program segitiga
+
+def segitiga():
+    print('==============================')
+    print('====== PROGRAM SEGITIGA ======')
+    print('==============================\n')
+
+
+    t = float(input('Masukan tinggi : '))
+    a = float(input('Masukan alas : '))
+    luas = lambda a,t : 0.5 * (a * t)
+    keliling = lambda a : a * a * a
+
+    print(f'luas : {luas(a,t)} cm2')
+    print(f'Keliling : {keliling(a)} cm2\n')
+
+segitiga()
