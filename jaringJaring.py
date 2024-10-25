361bb0d3dee09f820593517b7bccfe6db0924348
= <=>
2024-10-25 19:15:58 +0700

    Program jaring jaring

---
+++ jaringJaring.py
@@ -0,0 +1,17 @@
+# dibuat oleh : m.deryl fabiensyah
+# tanggal di buat : 24/10/2024
+# program rumus jaring jaring
+
+print('==========================')
+print('====== JARING JARING =====')
+print('==========================')
+
+r = int(input('masukan jari jari : '))
+s = int(input('masukan sisi miring : '))
+t = int(input('masukan tinggi : '))
+
+ls = 3.14 * r * s
+lp = (3.14 * r * s) + (3.14 * r * r)
+v = 1/3 * 3.14 * r * r * t
+
+print(f'jadi luas selimutnya adalah {ls} luas permukaannya adalah {lp} dan volumenya adalah {v}')
