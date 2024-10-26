54ba65beef44db23e7154045affc8db156aa77f5
= <=>
2024-10-26 22:38:07 +0700

    Program looping 1

---
+++ looping1.py
@@ -0,0 +1,23 @@
+# dibuat oleh : m.deryl fabiensyah
+# tanggal di buat : 25/10/2024
+# program looping
+
+# Looping 1
+
+print('*'*30)
+print('\tPROGRAM LOOPING')
+print('*'*30)
+
+for a in range(4):
+    print("Cerdas", end=" ")
+
+print('\n')
+
+for b in range(4):
+    print('Cerdas', end='\n')
+    
+print(' ')
+
+list = ['A.','B.','C.','D.']
+for a in list:
+    print(a,'Cerdas' ,end='\n')
