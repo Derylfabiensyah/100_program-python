f24cc5315c7a1e37741b8279e783e6e5e57333ef
= <=>
2024-10-27 20:46:51 +0700

    Program output 2

---
+++ output2.py
@@ -0,0 +1,9 @@
+# dibuat oleh : m.deryl fabiensyah
+# tanggal di buat : 27/10/2024
+# program output 2
+
+print('******************************************')
+print('              PROGRAM PYTHON')
+print('          oleh : Deryl Fabiensyah')
+print('             kelas : X PPLG 2')
+print('******************************************')
