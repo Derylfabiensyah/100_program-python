b7b41b57b5eebe82b9eba61745ecbb4dd78f19e6
= <=>
2024-10-27 20:46:39 +0700

    Program output 1

---
+++ output.py
@@ -0,0 +1,7 @@
+# dibuat oleh : m.deryl fabiensyah
+# tanggal di buat : 27/10/2024
+# program output 1
+
+print('Hello World')
+print('============================')
+print('Hello Algoritma ------------\n')
