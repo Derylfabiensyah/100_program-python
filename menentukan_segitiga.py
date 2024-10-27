7e5a35d547bedd385eaffcec6171f6724be35427
= <=>
2024-10-27 20:48:11 +0700

    Program menetukan segitiga

---
+++ menentukan_segitiga.py
@@ -0,0 +1,20 @@
+# dibuat oleh : m.deryl fabiensyah
+# tanggal di buat : 27/10/2024
+# program menentukan segitiga
+
+print('='*20)
+print('SEGITIGA')
+print('='*20)
+
+a = int(input('masukan sisi pertama : '))
+b = int(input('masukan sisi kedua : '))
+c = int(input('masukan sisi ketiga : '))
+
+if a**2 + b**2 == c**2:
+    print('ini termasuk kedalam segitiga siku siku')
+elif a**2 + b**2 > c**2:
+    print('ini termasuk kedalam segitiga lancip')
+elif a**2 + b**2 < c**2:
+    print('ini termasuk kedalam segitiga tumpul')
+else:
+    print('ini bukan segitiga')
