84b63c9498b8f15736366a1908331b6e64300b90
= <=>
2024-11-07 13:22:46 +0700

    Program fungsi upper

---
+++ fungsi_upper.py
@@ -0,0 +1,11 @@
+# dibuat oleh : m.deryl fabiensyah
+# tanggal di buat : 05/11/2024
+# program fungsi upper
+
+print('='*20)
+print('Fungsi upper')
+print('='*20)
+
+teks = input('Masukan teks : ')
+teks_kecil = teks.upper()
+print(teks_kecil)
