021d93ca6429f906e26c3669300390b78e99ff6f
= <=>
2024-11-07 13:20:39 +0700

    Program energi kinetik

---
+++ energiKinetik.py
@@ -0,0 +1,14 @@
+# dibuat oleh : m.deryl fabiensyah
+# tanggal di buat : 06/11/2024
+# program energi kinetik
+
+print('='*30)
+print('Menghitung energi kinetik')
+print('='*30)
+
+massa = int(input('Masukan massa : '))
+v = int(input('Masukan kecepatan : '))
+
+ek = 1/2 * massa * v * v
+
+print(ek)
