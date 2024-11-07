92b2ae46ecef072cf9c29b715a1411a03be49828
= <=>
2024-11-07 13:23:18 +0700

    Program hari besok

---
+++ hari_besok.py
@@ -0,0 +1,26 @@
+# dibuat oleh : m.deryl fabiensyah
+# tanggal di buat : 05/11/2024
+# program hari besok
+
+print('='*20)
+print('HARI BESOK')
+print('='*20)
+
+hari = input('Masukan nama hari : ')
+
+if hari == 'senin':
+    print('Besoknya selasa')
+elif hari == 'selasa':
+    print('Besoknya rabu')
+elif hari == 'rabu':
+    print('Besoknya kamis')
+elif hari == 'kamis':
+    print('Besoknya jumat')
+elif hari == 'jumat':
+    print('Besoknya sabtu')
+elif hari == 'sabtu':
+    print('Besoknya minggu')
+elif hari == 'minggu':
+    print('Besoknya senin')
+else:
+    print('ini bukan nama hari')
