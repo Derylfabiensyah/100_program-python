9cde6d6ef59b8044a8dc1e6dfb121cf07079c3dc
= <=>
2024-11-07 13:22:24 +0700

    Program fungsi lower

---
+++ fungsi_lower.py
@@ -0,0 +1,12 @@
+# dibuat oleh : m.deryl fabiensyah
+# tanggal di buat : 05/11/2024
+# program fungsi lower
+
+print('='*20)
+print('Fungsi lower')
+print('='*20)
+
+teks = input('Masukan teks : ')
+teks_kecil = teks.lower()
+print(teks_kecil)
+print()
