b7d466c13f58c0bc1317b73daa68816ef48cba6c
= <=>
2024-11-07 13:21:57 +0700

    Program energi potensial

---
+++ energiPotensial.py
@@ -0,0 +1,15 @@
+# dibuat oleh : m.deryl fabiensyah
+# tanggal di buat : 06/11/2024
+# program energi potensial
+
+print('='*30)
+print('Rumus energi potensial')
+print('='*30)
+
+m = int(input('Masukan massa benda : '))
+g = 10
+h = int(input('Masukan ketinggian benda : '))
+
+ep = m * g * h
+
+print(f'Energi potensialnya adalah : {ep}')
