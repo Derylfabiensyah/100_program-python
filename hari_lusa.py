679bbac5e75fb1f27beef85a6591fa4ba093bd52
= <=>
2024-11-07 13:25:22 +0700

    Program hari lusa

---
+++ hari_lusa.py
@@ -0,0 +1,26 @@
+# dibuat oleh : m.deryl fabiensyah
+# tanggal di buat : 05/11/2024
+# program hari lusa
+
+print("="*20)
+print("HARI LUSA")
+print("="*20)
+
+hari = input("Masukan nama hari : ")
+
+if hari == "senin":
+    print("Lusa adalah rabu")
+elif hari == "selasa":
+    print("Lusa adalah kamis")
+elif hari == "rabu":
+    print("Lusa adalah jumat")
+elif hari == "kamis":
+    print("Lusa adalah sabtu")
+elif hari == "jumat":
+    print("Lusa adalah minggu")
+elif hari == "sabtu":
+    print("Lusa adalah senin")
+elif hari == "minggu":
+    print("Lusa adalah selasa")
+else:
+    print("ini bukan nama hari")
