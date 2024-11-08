58f79e0081755ce041e09b0c6c26eb1a1d5b35fb
= <=>
2024-11-08 07:30:49 +0700

    Program nilai terkecil

---
+++ nilaiTerkecil.py
@@ -0,0 +1,15 @@
+# dibuat oleh : m.deryl fabiensyah
+# tanggal di buat : 07/10/2024
+# program nilai terkecil
+
+print('='*40)
+print('MENCARI NILAI TERKECIL')
+print('='*40)
+
+n1 = int(input('masukan nilai kesatu : '))
+n2 = int(input('masukan nilai kedua : '))
+n3 = int(input('masukan nilai ketiga : '))
+
+kecil = min(n1,n2,n3)
+
+print(f'nilai terkecil adalah {kecil}')
