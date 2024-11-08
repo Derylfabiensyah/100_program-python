881b30cb523203b9345b148e054e7e155835a9a3
= <=>
2024-11-08 07:31:38 +0700

    Program pilihan ganda

---
+++ pilihan_ganda.py
@@ -0,0 +1,16 @@
+# dibuat oleh : m.deryl fabiensyah
+# tanggal di buat : 07/10/2024
+# program tanya jawab
+
+print('siapa orang yang menemukan hukum newton?')
+print('''
+      a. Deryl
+      b. Arsa
+      c. Isaac Newton
+      d. Kiano
+      e. Dika''')
+a=(input('masukan jawaban : '))
+if a == 'c':
+    print ('bener')
+else:
+    print ('salah,jawaban nya adalah c')
