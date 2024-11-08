0a60ba473a6653bcffe5c477fc293b8a0ab47209
= <=>
2024-11-08 07:26:34 +0700

    Program kecepatan berkendara

---
+++ kecepatan_berkendara.py
@@ -0,0 +1,18 @@
+# dibuat oleh : m.deryl fabiensyah
+# tanggal di buat : 07/10/2024
+# program menghitung kecepatan
+
+print("="*40)
+print("PROGRAM MENGHITUNG KECEPATAN KENDARAAN")
+print("="*40)
+
+
+try:
+    jarak = int(input("masukan jarak berkendara (m) = "))
+    waktu = int(input("masukan waktu berkendara (s) = "))
+    kecepatan = jarak / waktu
+    print(f"kecepatan kendaraan anda adalah = {kecepatan:.2f} m/s")
+
+except ValueError:
+    print("inputan anda tidak valid")
+    print("silahka masukan y/t saja")
