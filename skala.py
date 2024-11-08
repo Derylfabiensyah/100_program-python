f77d140262f76f98b284d5dbb50420b4cb248f7c
= <=>
2024-11-08 07:32:53 +0700

    Program skala peta

---
+++ skala.py
@@ -0,0 +1,11 @@
+# dibuat oleh : m.deryl fabiensyah
+# tanggal di buat : 07/10/2024
+# program skala peta
+
+print('='*30)
+print('MENGHITUNG SKALA')
+print('='*30)
+
+jarak = float(input("Masukan jarak pada peta (dalam cm) :"))
+Skala = float(input("masukan Skala peta: 1/"))
+print (jarak*Skala/100000,"KM")
