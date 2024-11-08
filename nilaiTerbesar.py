ef2005e390263a6f41f4281561b03b7faddbfde9
= <=>
2024-11-08 07:29:03 +0700

    Program nilai terbesar

---
+++ nilaiTerbesar.py
@@ -0,0 +1,15 @@
+# dibuat oleh : m.deryl fabiensyah
+# tanggal di buat : 07/10/2024
+# program nilai terbesar
+
+print('='*40)
+print('MENCARI NILAI TERBESAR')
+print('='*40)
+
+n1 = int(input('masukan nilai kesatu : '))
+n2 = int(input('masukan nilai kedua : '))
+n3 = int(input('masukan nilai ketiga : '))
+
+besar = max(n1,n2,n3)
+
+print(f'nilai terbesar adalah {besar}')
