ae6893064822accc1edd1b358b3ce386e5414a65
= <=>
2024-11-13 19:39:06 +0700

    Program looping angka 2

---
+++ looping_angka2.py
@@ -0,0 +1,14 @@
+# dibuat oleh : m.deryl fabiensyah
+# tanggal di buat : 12/11/2024
+# program looping angka 2
+
+print('='*20)
+print('LOOPING ANGKA 2')
+print('='*20)
+
+c = 1
+while c < 6:
+    print(c)
+    c = c + 1
+print('------+')
+print(15)
