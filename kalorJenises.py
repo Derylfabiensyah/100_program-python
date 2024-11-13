f5938cf10cdbcd9a4c95376be80a6549c716b5ac
= <=>
2024-11-13 19:12:55 +0700

    Program kalor jenis es

---
+++ kalorJenises.py
@@ -0,0 +1,15 @@
+# dibuat oleh : m.deryl fabiensyah
+# tanggal di buat : 12/11/2024
+# program kalor jenis es
+
+print('='*35)
+print("KALOR JENIS ES")
+print('='*35)
+
+m = float(input("masukan massa es = "))
+suhu_awal = float(input("masukan suhu awal = "))
+suhu_akhir = float(input("masukan suhu akhir = "))
+kalor_jenis_es = 2.1
+t = suhu_awal - suhu_akhir
+q1 = m * kalor_jenis_es * t
+print(f"julmah kalor yang diperlukan adalah {q1}")
