456fd13295a820ed0fa81a03867e52e2bd5e3060
= <=>
2024-11-13 19:40:22 +0700

    Program looping cerdas 1

---
+++ looping_cerdas1.py
@@ -0,0 +1,10 @@
+# dibuat oleh : m.deryl fabiensyah
+# tanggal di buat : 12/11/2024
+# program looping cerdas 1
+
+print('='*20)
+print('LOOPING CERDAS 1')
+print('='*20)
+
+for a in range(4):
+    print("Cerdas", end=" ")
