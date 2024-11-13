22972d12b0788f69075bbb6fd06785b4a7bb498f
= <=>
2024-11-13 19:39:55 +0700

    Program looping bintang 2

---
+++ looping_bintang2.py
@@ -0,0 +1,12 @@
+# dibuat oleh : m.deryl fabiensyah
+# tanggal di buat : 12/11/2024
+# program looping bintang 2
+
+print('='*20)
+print('LOOPING BINTANG 2')
+print('='*20)
+
+l = 5
+for b in range(5):
+    print('*'*l)
+    l -= 1
