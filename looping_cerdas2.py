fa9cce18983132e8903e13d82b60eacf9026d713
= <=>
2024-11-13 19:40:32 +0700

    Program looping cerdas 2

---
+++ looping_cerdas2.py
@@ -0,0 +1,10 @@
+# dibuat oleh : m.deryl fabiensyah
+# tanggal di buat : 12/11/2024
+# program looping cerdas 2
+
+print('='*20)
+print('LOOPING CERDAS 2')
+print('='*20)
+
+for b in range(4):
+    print('Cerdas', end='\n')
