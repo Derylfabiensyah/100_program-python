e49c2ada38941075a79b6d9f545fe069f177940f
= <=>
2024-11-13 19:08:06 +0700

    Program angka romawi 2

---
+++ angkaRomawi2.py
@@ -0,0 +1,20 @@
+# dibuat oleh : m.deryl fabiensyah
+# tanggal di buat : 12/11/2024
+# Program angka romawi
+
+print('='*20)
+print('ANGKA ROMAWI')
+print('='*20)
+
+n = int(input("Masukkan Bilangan Bulat Positif : "))
+romawi = [
+    (1000, "M"), (900, "CM"), (400, "CD"), (100, "C"), (90, "XC"), (50, "L"), 
+    (40, "XL"), (10, "X"), (9, "IX"), (5, "V"), (4, "IV"), (1, "I")
+]
+hasil = ""
+for nilai, simbol in romawi:
+    while n >= nilai:
+        hasil += simbol
+        n -= nilai
+
+print(f'Angka romawi dari {n} ialah {hasil}')
