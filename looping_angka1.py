5845e5b4b249453648450c2b095989b53c133d35
= <=>
2024-11-13 19:38:54 +0700

    Program looping angka 1

---
+++ looping_angka1.py
@@ -0,0 +1,13 @@
+# dibuat oleh : m.deryl fabiensyah
+# tanggal di buat : 12/11/2024
+# program looping angka 1
+
+print('='*20)
+print('LOOPING ANGKA 1')
+print('='*20)
+
+a = 1
+while a < 6:
+    print(a, end=' ')
+    a = a + 1
+print('= 15')
