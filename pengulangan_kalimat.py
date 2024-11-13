f7ff61ca9f8805fa9f92c705fe3ebf1343fe85b6
= <=>
2024-11-13 19:47:59 +0700

    Program looping kalimat

---
+++ pengulangan_kalimat.py
@@ -0,0 +1,16 @@
+# dibuat oleh : m.deryl fabiensyah
+# tanggal di buat : 11/11/2024
+# program looping kalimat
+
+print('='*40)
+print("PROGRAM PEGULAGAN KALIMAT / KATA")
+print('='*40)
+
+try:
+    input_pesan = str(input("masukan pesan yang akan di ulang : "))
+    input_no = int(input("berapa kali ingin di ulang : "))
+    for i in range(input_no):
+        print(f"{i+1}.{input_pesan}")
+
+except ValueError:
+    print("inputan anda tidak valid")
