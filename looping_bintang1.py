75ef9657d6f39591b7a4f5e9f348947b6669bc0b
= <=>
2024-11-13 19:39:44 +0700

    Program looping bintang 1

---
+++ looping_bintang1.py
@@ -0,0 +1,11 @@
+# dibuat oleh : m.deryl fabiensyah
+# tanggal di buat : 12/11/2024
+# program looping bintang 1
+
+print('='*20)
+print('LOOPING BINTANG 1')
+print('='*20)
+
+for a in range(6):
+    print("*"*a)
+    a += 1
