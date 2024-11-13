4164f87f7fe2227431bcc3309def3089ee10a49e
= <=>
2024-11-13 19:42:31 +0700

    Program menentukan nilai abc

---
+++ nilai_abc.py
@@ -0,0 +1,14 @@
+# dibuat oleh : m.deryl fabiensyah
+# tanggal di buat : 11/11/2024
+# program menentuka nilai abc
+
+print(35*"=")
+print("PROGRAM ABC")
+print(35*"=")
+
+a = float(input("masukan nilai a = "))
+b = float(input("masukan nilai b = "))
+c = float(input("masukan nilai c = "))
+
+x = (-b) + 2  * (c**2) * + 4 * a * b / 2 * c 
+print(f"hasil dari rumus abc adalah = {x:.2f}")
