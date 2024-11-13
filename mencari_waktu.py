fcaebccd2c5e0f81c3494b3b95f52dfcb81e53ab
= <=>
2024-11-13 19:41:20 +0700

    Program mencari waktu

---
+++ mencari_waktu.py
@@ -0,0 +1,12 @@
+# dibuat oleh : m.deryl fabiensyah
+# tanggal di buat : 12/11/2024
+# program mencari waktu
+
+print('='*40)
+print('Program Mencari Waktu')
+print('='*40)
+
+s=int(input('masukan Jarak (S)'))
+v=int(input('masukan Kecepatan (v)'))
+t=s/v
+print(f'Waktu yang di butuhkan adalah{t}')
