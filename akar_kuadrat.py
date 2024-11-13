03f72cdd82a702c0f87add355f16de4984ad1a3d
= <=>
2024-11-13 19:06:55 +0700

    Program akar kuadrat

---
+++ akar_kuadrat.py
@@ -0,0 +1,11 @@
+# dibuat oleh : m.deryl fabiensyah
+# tanggal di buat : 12/11/2024
+# Program akar kuadrat
+
+print('='*25)
+print('PROGRAM AKAR KUADRAT')
+print('='*25)
+
+angka = float(input('Masukkan angka : '))
+akar_kuadrat = angka ** 0.5
+print(f'Akar kuadrat dari {angka} adalah {akar_kuadrat}')
